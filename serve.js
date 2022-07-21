13ffb2db9a2c5fe858afb2c57752fdd8e36543c6
Ehsan Mahmoudi <me@e-mahmoudi.me>
2022-07-21 10:12:24 +0430

    config file (#46)

--- serve.js
+++ serve.js
@@ -1,30 +1,10 @@
-import 'dotenv/config';
+import config from './src/config'
 import {app} from './src/index.js';
 import Debug from 'debug';
 
 const debug = Debug('tolling:server');
-const port = normalizePort(process.env['PORT']);
-app.listen(port, () => {
-  debug(`API server started on :${port}`);
-  console.log(`API server started on :${port}`);
+
+app.listen(config.port, () => {
+  debug(`API server started on :${config.port}`);
+  console.log(`API server started on :${config.port}`);
 });
-
-/**
- * Normalize a port into a number, string, or false.
- */
-
-function normalizePort(val) {
-  var port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}

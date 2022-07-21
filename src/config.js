acebfcc7eba2159a7371a4fbd464356b357c76e6
Jeyem <me@e-mahmoudi.me>
2022-07-21 09:53:28 +0430

    config file

---
+++ src/config.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+
+module.exports = {
+  port: normalizePort(process.env.PORT),
+};
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}

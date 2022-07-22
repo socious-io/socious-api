d6d16a712b85963cab6eeb72511cb865b6a5ba1f
Jeyem <me@e-mahmoudi.me>
2022-07-22 14:02:59 +0430

    comment for create OTP retry confusion

--- src/models/User/write.js
+++ src/models/User/write.js
@@ -17,7 +17,17 @@ export const verifyOTP = async (id) => {
   await app.db.query(sql`UPDATE otps SET verified_at=now() WHERE id=${id}`);
 };
 
+
+/*
+  As we generate 6 digit number as OTP code and in OTP table code constraint to be unique
+  there is no garanty everytime we generate this would be unique
+  this retry make sure we don't get any problem to saving otps record
+  still has very very tiny chance to make 5 time exists code on long term
+  TODO plan would be create a PSQL function to clean otps table old codes
+  and schdule to run these cleaner daily bases.
+*/
 export const createOTP = async (userId, retried = 0) => {
+  // generate random 6 digit number
   const code = Math.floor(100000 + Math.random() * 900000);
   try {
     await app.db.query(

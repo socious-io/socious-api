c68db65a9468974f6deb556b6c73a8edb27bc07b
Jeyem <me@e-mahmoudi.me>
2022-07-22 20:36:48 +0430

    otp type

--- src/models/User/write.js
+++ src/models/User/write.js
@@ -1,6 +1,7 @@
 import sql from 'sql-template-tag';
 import {app} from '../../index.js';
 import {EntryError} from '../../utils/errors.js';
+import { UserStatus } from './enum.js';
 
 export const insert = async (username, email, hashedPasswd) => {
   try {
@@ -12,6 +13,14 @@ export const insert = async (username, email, hashedPasswd) => {
     throw new EntryError(err.message);
   }
 };
+
+export const verifyEmail = async (id) => {
+  await app.db.query(sql`UPDATE users SET email_verified_at=now(),status=${UserStatus.ACTIVE} WHERE id=${id}`);
+}
+
+export const verifyPhone = async (id) => {
+  await app.db.query(sql`UPDATE users SET phone_verified_at=now(),status=${UserStatus.ACTIVE} WHERE id=${id}`);
+}
 
 export const verifyOTP = async (id) => {
   await app.db.query(sql`UPDATE otps SET verified_at=now() WHERE id=${id}`);
@@ -26,12 +35,12 @@ export const verifyOTP = async (id) => {
   TODO plan would be create a PSQL function to clean otps table old codes
   and schdule to run these cleaner daily bases.
 */
-export const createOTP = async (userId, retried = 0) => {
+export const createOTP = async (userId, otpType, retried = 0) => {
   // generate random 6 digit number
   const code = Math.floor(100000 + Math.random() * 900000);
   try {
     await app.db.query(
-      sql`INSERT INTO otps (code, user_id) VALUES (${code}, ${userId})`,
+      sql`INSERT INTO otps (code, user_id, type) VALUES (${code}, ${userId}, ${otpType})`,
     );
   } catch (e) {
     retried++;

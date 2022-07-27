3dea27fd184c7b2579c600e6000f30ccddbde628
Jeyem <me@e-mahmoudi.me>
2022-07-27 18:16:41 +0430

    constraint to members refrences

--- migrations/sqls/20220727075822-org-members-up.sql
+++ migrations/sqls/20220727075822-org-members-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS public.org_members (
   id uuid DEFAULT public.uuid_generate_v4() PRIMARY KEY NOT NULL,
-  user_id uuid,
-  org_id uuid,
+  user_id uuid NOT NULL,
+  org_id uuid NOT NULL,
   created_at timestamp with time zone DEFAULT now() NOT NULL,
   CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   CONSTRAINT fk_org FOREIGN KEY (org_id) REFERENCES organization(id) ON DELETE CASCADE

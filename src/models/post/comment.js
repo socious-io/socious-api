553530a61de8fc6fda954821fddf1c2de0a4a3e7
Jeyem <me@e-mahmoudi.me>
2022-08-22 11:12:54 +0430

    posts comments

---
+++ src/models/post/comment.js
@@ -0,0 +1,63 @@
+import sql from 'sql-template-tag';
+import {app} from '../../index.js';
+import {upsertCommentSchem} from './schema.js';
+import {EntryError} from '../../utils/errors.js';
+
+export const newComment = async (id, identityId, body) => {
+  await upsertCommentSchem.validateAsync(body);
+  try {
+    const {rows} = await app.db.query(sql`
+    INSERT 
+      INTO comments (identity_id, post_id, content, reply_id)
+      VALUES(${identityId}, ${id}, ${body.content}, ${body.reply_id})
+      RETURNING *
+    `);
+    return rows[0];
+  } catch (err) {
+    throw new EntryError(err.message);
+  }
+};
+
+export const updateComment = async (id, identityId, body) => {
+  await upsertCommentSchem.validateAsync(body);
+  try {
+    const {rows} = await app.db.query(sql`
+    UPDATE comments SET
+      content=${body.content}
+    WHERE id=${id} AND identity_id=${identityId}
+    RETURNING *
+    `);
+    return rows[0];
+  } catch (err) {
+    throw new EntryError(err.message);
+  }
+};
+
+export const removeComment = async (id, identityId) => {
+  return app.db.query(
+    sql`DELETE FROM comments WHERE id=${id} AND identity_id=${identityId}`,
+  );
+};
+
+export const comments = async (id, {offset = 0, limit = 10}) => {
+  const {rows} = await app.db.query(sql`
+  SELECT 
+  COUNT(*) OVER () as total_count,
+  c.*, i.type  as identity_type, i.meta as identity_meta
+  FROM comments c JOIN identities i ON c.identity_id=i.id
+  WHERE post_id=${id} AND reply_id IS NULL
+  ORDER BY created_at DESC  LIMIT ${limit} OFFSET ${offset}
+  `);
+  return rows;
+};
+
+export const commentsReplies = async (id, {offset = 0, limit = 10}) => {
+  const {rows} = await app.db.query(sql`
+  SELECT COUNT(*) OVER () as total_count,
+  c.*, i.type  as identity_type, i.meta as identity_meta
+  FROM comments c JOIN identities i ON c.identity_id=i.id
+  WHERE reply_id=${id}
+  ORDER BY created_at DESC  LIMIT ${limit} OFFSET ${offset}
+  `);
+  return rows;
+};
--- src/models/post/index.js
+++ src/models/post/index.js
@@ -1,7 +1,9 @@
 import * as write from './write.js';
 import * as read from './read.js';
+import * as comment from './comment.js';
 
 export default {
   ...read,
   ...write,
+  ...comment,
 };

1775ad30315be623440360499bc9a84a34a9cad8
Jeyem <me@e-mahmoudi.me>
2022-08-24 23:45:45 +0430

    identities improve #EGN-560

--- src/routes/identity.js
+++ src/routes/identity.js
@@ -5,6 +5,23 @@ import {identity} from '../utils/requests.js';
 export const router = new Router();
 
 /**
+ * @api {get} /identities Get Current
+ * @apiGroup Identity
+ * @apiName Current
+ * @apiVersion 2.0.0
+ * @apiDescription get others identity
+ *
+ * @apiHeader {String} Current-Identity default current user identity can set organization identity if current user has permission
+ *
+ * @apiSuccess (200) {String} id
+ * @apiSuccess (200) {Object} type (users, organizations)
+ * @apiSuccess (200) {Object} meta
+ */
+router.get('/', identity, async (ctx) => {
+  ctx.body = await Identity.getAll(ctx.user.id, ctx.identity.id);
+});
+
+/**
  * @api {get} /identities/:id Get Others
  * @apiGroup Identity
  * @apiName GetOthers
@@ -19,41 +36,6 @@ export const router = new Router();
  */
 router.get('/:id', async (ctx) => {
   ctx.body = await Identity.get(ctx.params.id);
-});
-
-/**
- * @api {post} /identities/batch Get Others Multi
- * @apiGroup Identity
- * @apiName GetOthersMulti
- * @apiVersion 2.0.0
- * @apiDescription get others identity multi
- *
- * @apiBody {String[]{max:10}} ids
- *
- * @apiSuccess (200) {Object[]} items
- * @apiSuccess (200) {String} items.id
- * @apiSuccess (200) {String} items.type (users, organizations)
- * @apiSuccess (200) {Object} items.meta
- */
-router.post('/batch', async (ctx) => {
-  ctx.body = {items: await Identity.getAll(ctx.request.body)};
-});
-
-/**
- * @api {get} /identities Get Current
- * @apiGroup Identity
- * @apiName Current
- * @apiVersion 2.0.0
- * @apiDescription get others identity
- *
- * @apiHeader {String} Current-Identity default current user identity can set organization identity if current user has permission
- *
- * @apiSuccess (200) {String} id
- * @apiSuccess (200) {Object} type (users, organizations)
- * @apiSuccess (200) {Object} meta
- */
-router.get('/', identity, async (ctx) => {
-  ctx.body = ctx.identity;
 });
 
 /**

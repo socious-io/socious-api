b9f2d7a5ce186c6218aa4a191e3a4b848a6ec5bd
Lalo Martins <lalo.martins@gmail.com>
2022-09-07 12:08:31 +0300

    fix crashing when no current-identity header

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -36,7 +36,7 @@ export const identity = async (ctx, next) => {
 
   await Identity.permissioned(identity, ctx.user.id);
 
-  ctx.identity = identity.toLowerCase();
+  ctx.identity = identity?.toLowerCase();
 
   await next();
 };

e2f620bd423651d33d470d974bc7d8b1c540c386
Jeyem <me@e-mahmoudi.me>
2022-09-20 22:08:23 +0430

    notifications types

--- src/models/notification/index.js
+++ src/models/notification/index.js
@@ -1,7 +1,9 @@
 import * as write from './write.js';
 import * as read from './read.js';
+import Data from '@socious/data'
 
 export default {
+  Types: Data.NotificationType,
   ...read,
   ...write,
 };

a6c0ce5355699e48c0fbbbbfef913aa8d0f832eb
Lalo Martins <lalo.martins@gmail.com>
2022-10-09 09:59:50 +0300

    Remove duplicate change from migration

--- migrations/sqls/20220907052546-project-add-columns-for-other-parties-up.sql
+++ migrations/sqls/20220907052546-project-add-columns-for-other-parties-up.sql
@@ -1,4 +1,3 @@
-ALTER TYPE project_remote_preference_type RENAME VALUE 'REMOOTE' TO 'REMOTE';
 ALTER TABLE projects 
 ADD COLUMN other_party_id varchar(60) DEFAULT NULL, 
 ADD COLUMN other_party_title varchar(250) DEFAULT NULL, 

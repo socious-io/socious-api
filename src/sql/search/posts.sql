98bae1654467b0d73b6b4c178b67768d867c5d51
Jeyem <me@e-mahmoudi.me>
2022-10-11 10:28:18 +0330

    search project issue with apidoc description

--- src/sql/search/posts.sql
+++ src/sql/search/posts.sql
@@ -1,9 +1,6 @@
 SELECT
   p.id
 FROM posts p
-JOIN identities i ON i.id=p.identity_id
-LEFT JOIN users u ON u.id=i.id and i.type='users'
-LEFT JOIN organizations o ON o.id=i.id and i.type='organizations'
 WHERE  
   p.search_tsv @@ websearch_to_tsquery($1)
   {{filter}}

434e659566efb1d714f91b210a028f735eb12a78
Jeyem <me@e-mahmoudi.me>
2022-10-24 11:47:41 +0330

    profile recomend

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,29 +3,27 @@ import Project from '../models/project/index.js';
 import Post from '../models/post/index.js';
 import Org from '../models/organization/index.js';
 import User from '../models/user/index.js';
-import {
-  loginOptional,
-} from '../utils/middlewares/authorization.js';
+import {loginOptional} from '../utils/middlewares/authorization.js';
 
 export const router = new Router();
 
-router.get('/options', loginOptional,  async (ctx) => {
+router.get('/options', loginOptional, async (ctx) => {
   ctx.body = {
     projects: {
       filter_columns: Object.keys(Project.filterColumns),
-      sort_columns: Project.sortColumns
+      sort_columns: Project.sortColumns,
     },
     posts: {
       filter_columns: Object.keys(Post.filterColumns),
-      sort_columns: Post.sortColumns
+      sort_columns: Post.sortColumns,
     },
     organizations: {
       filter_columns: Object.keys(Org.filterColumns),
-      sort_columns: Org.sortColumns
+      sort_columns: Org.sortColumns,
     },
     users: {
       filter_columns: Object.keys(User.filterColumns),
-      sort_columns: User.sortColumns
-    }
-  }
+      sort_columns: User.sortColumns,
+    },
+  };
 });

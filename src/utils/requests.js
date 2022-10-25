03fbe5fa51c980f935887b40b967f685f40269e8
Jeyem <me@e-mahmoudi.me>
2022-10-25 09:25:16 +0330

    make filters BC

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,8 +1,13 @@
 const filters = (ctx) => {
   const result = {};
 
-  for (const [key, val] of Object.entries(ctx.query)) {
+  for (let [key, val] of Object.entries(ctx.query)) {
+    // for BC 2 filter routes
+    if (key === 'status') key = 'filter.status'
+    if (key === 'identity_id') key = 'filter.identity_id'
+
     if (!key.includes('filter.')) continue;
+    
     result[key.split('filter.')[1]] = val;
   }
 
@@ -16,7 +21,6 @@ export const paginate = async (ctx, next) => {
   let limit = parseInt(ctx.query.limit) || 10;
   if (limit < 1) limit = 10;
 
-  // TODO: we can handle ordering system here
 
   ctx.paginate = {
     page,

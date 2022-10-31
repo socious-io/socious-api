8bb005fbb292842cf7f0585f62dfcae365762045
Jeyem <me@e-mahmoudi.me>
2022-10-31 23:27:23 +0330

    split on not array inputs

--- src/utils/query.js
+++ src/utils/query.js
@@ -30,9 +30,11 @@ export const filtering = (filter, columns, append = true, prefix = '') => {
     let op = operators[valueKeys[0]];
 
     let value = op ? val[valueKeys[0]] : val;
-
-    const splited = value.split(',');
-    if (splited.length > 1) value = splited;
+    
+    if (!Array.isArray(value)) {
+      const splited = value.split(',');
+      if (splited.length > 1) value = splited;
+    }
 
     if (columns[key] === Array) {
       value = `{${value}}`;

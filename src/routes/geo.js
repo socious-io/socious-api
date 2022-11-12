3f5327145a759e47a5f21087a0aeb9fdece6760c
Lalo Martins <lalo.martins@gmail.com>
2022-11-12 17:31:56 +0200

    geoloc API final: search

--- src/routes/geo.js
+++ src/routes/geo.js
@@ -1,6 +1,9 @@
 import Router from '@koa/router';
 import {geoip} from '../services/geo/geoip.js';
-import {locationsByCountry} from '../services/geo/geoname.js';
+import {
+  locationsByCountry,
+  locationsSearchByCountry,
+} from '../services/geo/geoname.js';
 import {paginate} from '../utils/requests.js';
 
 export const router = new Router();
@@ -13,6 +16,12 @@ router.get('/locations/country/:countryCode', paginate, async (ctx) => {
   const {countryCode} = ctx.params;
   if (countryCode?.length !== 2 || countryCode === 'XW') {
     ctx.body = [];
+  } else if (ctx.query.search) {
+    ctx.body = await locationsSearchByCountry(
+      countryCode,
+      ctx.query.search,
+      ctx.paginate,
+    );
   } else {
     ctx.body = await locationsByCountry(countryCode, ctx.paginate);
   }
--- src/services/geo/geoname.js
+++ src/services/geo/geoname.js
@@ -31,3 +31,40 @@ export async function locationsByCountry(
   `);
   return rows;
 }
+
+export async function locationsSearchByCountry(
+  countryCode,
+  search,
+  {offset = 0, limit = 10, filter, sort},
+) {
+  const searchPat = `%${search}%`;
+  const {rows} = await app.db.query(sql`SELECT COUNT(*) OVER () as total_count,
+    loc.id as id, loc.name as name, loc.feature_code as type, loc.population as population, loc.country_code as country_code,
+    loc.admin1_code as region_id, adm.name as region_name
+    FROM geonames loc
+    LEFT JOIN geonames adm ON adm.feature_code = 'ADM1' AND adm.admin1_code = loc.admin1_code AND adm.country_code = loc.country_code
+    WHERE loc.country_code = ${countryCode} AND loc.feature_class = 'P'
+    AND loc.name ILIKE ${searchPat}
+    ${filtering(filter, filterColumns, true)}
+    ${sorting(sort || 'name', sortColumns, 'loc')}
+    LIMIT ${limit} OFFSET ${offset}
+  `);
+  if (rows.length && rows[0].total_count >= limit) return rows;
+
+  const {rows: rowsAlt} = await app.db.query(sql`SELECT
+    COUNT(*) OVER () as total_count,
+    loc.id as id, loc.name as name, loc.feature_code as type, loc.population as population, loc.country_code as country_code,
+    alt.alternate_name, alt.iso_language as alt_language,
+    alt.is_historic, alt.is_colloquial, alt.is_short_name,
+    loc.admin1_code as region_id, adm.name as region_name
+    FROM geonames loc
+    LEFT JOIN geonames adm ON adm.feature_code = 'ADM1' AND adm.admin1_code = loc.admin1_code AND adm.country_code = loc.country_code
+    LEFT JOIN geonames_alt alt ON alt.geoname_id = loc.id
+    WHERE loc.country_code = ${countryCode} AND loc.feature_class = 'P'
+    AND (alt.alternate_name ILIKE ${searchPat} AND NOT loc.name ILIKE ${searchPat})
+    ${filtering(filter, filterColumns, true)}
+    ${sorting(sort || 'name', sortColumns, 'loc')}
+    LIMIT ${limit} OFFSET ${offset}
+  `);
+  return rows.concat(rowsAlt);
+}

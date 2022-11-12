02c45b6b1cbbc9c725276d6f76ee7ced0af89feb
Lalo Martins <lalo.martins@gmail.com>
2022-11-12 16:01:10 +0200

    geoloc API V2: region names for other countries

--- src/services/geo/geoname.js
+++ src/services/geo/geoname.js
@@ -4,10 +4,11 @@ import {app} from '../../index.js';
 export async function locationsByCountry(countryCode) {
   const {rows} = await app.db.query(sql`SELECT
     loc.id as id, loc.name as name, loc.feature_code as type, loc.population as pop, loc.country_code as country,
-    loc.admin1_code as region
+    loc.admin1_code as region_id, adm.name as region_name
     FROM geonames loc
-    WHERE country_code = ${countryCode} AND feature_class = 'P'
-    ORDER BY name ASC
+    LEFT JOIN geonames adm ON adm.feature_code = 'ADM1' AND adm.admin1_code = loc.admin1_code AND adm.country_code = loc.country_code
+    WHERE loc.country_code = ${countryCode} AND loc.feature_class = 'P'
+    ORDER BY loc.name ASC
   `);
   return rows;
 }

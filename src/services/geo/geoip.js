bf99513a87d36535d047197699e6760e3d755432
Lalo Martins <lalo.martins@gmail.com>
2022-11-12 20:40:37 +0200

    use official geoip library instead to get location id

--- src/services/geo/geoip.js
+++ src/services/geo/geoip.js
@@ -1,17 +1,37 @@
-import fgeoip from 'fast-geoip';
+import {Reader} from '@maxmind/geoip2-node';
+import config from '../../config.js';
+
+const reader = await Reader.open(config.geoipDb).catch((_e) => null);
 
 export async function geoip(ip) {
+  if (reader === null) return {};
+
+  let geo;
   try {
-    const geo = await fgeoip.lookup(ip);
-    return {
-      country: geo.country,
-      region: geo.region,
-      city: geo.city,
-      eu: geo.eu === '1',
-      timezone: geo.timezone,
-    };
+    geo = reader.city(ip);
   } catch (e) {
     console.error(e);
     return {error: 'not matched'};
   }
+  if (geo?.country == null) return {error: 'not matched'};
+
+  const resp = {
+    id: geo.city?.geonameId,
+    country: geo.country.isoCode,
+    region: geo.region,
+    city: geo.city?.names.en,
+    eu: geo.country.isInEuropeanUnion,
+    timezone: geo.location?.timezone,
+  };
+  if (geo.subdivisions?.length) {
+    resp.admin1_code = geo.subdivisions[0].isoCode;
+    resp.admin1_id = geo.subdivisions[0].geonameId;
+    resp.admin1_name = geo.subdivisions[0].names.en;
+    if (geo.subdivisions?.length > 1) {
+      resp.admin2_code = geo.subdivisions[1].isoCode;
+      resp.admin2_id = geo.subdivisions[1].geonameId;
+      resp.admin2_name = geo.subdivisions[1].names.en;
+    }
+  }
+  return resp;
 }

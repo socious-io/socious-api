04745cc39433f15af30822511d912808f46cb65a
Jeyem <me@e-mahmoudi.me>
2022-11-14 10:19:32 +0330

    add entities filter and sort on site options

--- src/routes/site.js
+++ src/routes/site.js
@@ -3,6 +3,9 @@ import Project from '../models/project/index.js';
 import Post from '../models/post/index.js';
 import Org from '../models/organization/index.js';
 import User from '../models/user/index.js';
+import Applicant from '../models/applicant/index.js';
+import Offer from '../models/offer/index.js';
+import Mission from '../models/mission/index.js';
 import {loginOptional} from '../utils/middlewares/authorization.js';
 
 export const router = new Router();
@@ -25,5 +28,17 @@ router.get('/options', loginOptional, async (ctx) => {
       filter_columns: Object.keys(User.filterColumns),
       sort_columns: User.sortColumns,
     },
+    applicants: {
+      filter_columns: Object.keys(Applicant.filterColumns),
+      sort_columns: Applicant.sortColumns,
+    },
+    missions: {
+      filter_columns: Object.keys(Mission.filterColumns),
+      sort_columns: Mission.sortColumns,
+    },
+    offers: {
+      filter_columns: Object.keys(Offer.filterColumns),
+      sort_columns: Offer.sortColumns,
+    }
   };
 });

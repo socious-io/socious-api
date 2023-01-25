516fda8dac5aecd7a1552294412d4b0d4cbf4dd1
jeyem <me@e-mahmoudi.me>
2023-01-25 19:44:10 +0330

    share directory

--- serve.js
+++ serve.js
@@ -1,8 +1,14 @@
 import Config from './src/config.js'
 import { app } from './src/index.js'
 import Debug from 'debug'
-
+import fs from 'fs/promises'
 const debug = Debug('tolling:server')
+
+fs.readFile('/var/lib/share/test.txt').then((r) => {
+  console.log('------------------------')
+  console.log(r.toString())
+  console.log('------------------------')
+})
 
 app.listen(Config.port, () => {
   debug(`API server started on :${Config.port}`)

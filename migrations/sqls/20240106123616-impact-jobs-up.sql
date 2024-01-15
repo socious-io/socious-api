0901ac74a360fde40561ccc4f9445576c98ab94f
jeyem <me@e-mahmoudi.me>
2024-01-15 09:55:20 +0330

    missed migration change for impact_job

--- migrations/sqls/20240106123616-impact-jobs-up.sql
+++ migrations/sqls/20240106123616-impact-jobs-up.sql
@@ -1,11 +1,12 @@
 ALTER TABLE projects ADD COLUMN impact_job boolean DEFAULT true;
 
-
 CREATE OR REPLACE FUNCTION active_by_impact_job()
-  RETURNS TRIGGER AS
+RETURNS TRIGGER AS
 $$
 BEGIN
-  UPDATE projects SET status='EXPIRE' WHERE id=NEW.id AND impact_job=false;
+  IF NEW.impact_job = FALSE THEN
+    NEW.status := 'EXPIRE';
+  END IF;
   RETURN NEW;
 END;
 $$
@@ -13,7 +14,7 @@ LANGUAGE PLPGSQL;
 
 
 CREATE TRIGGER update_project_on_impact
-    AFTER UPDATE ON projects FOR EACH ROW EXECUTE FUNCTION active_by_impact_job();
+    BEFORE UPDATE ON projects FOR EACH ROW EXECUTE FUNCTION active_by_impact_job();
 
-CREATE TRIGGER insert_identity
-    AFTER INSERT ON projects FOR EACH ROW EXECUTE FUNCTION active_by_impact_job();
+CREATE TRIGGER insert_project_on_impact
+    BEFORE INSERT ON projects FOR EACH ROW EXECUTE FUNCTION active_by_impact_job();

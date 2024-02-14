96b7707dfe931fb0f11e3a548df767376a576f7d
jeyem <me@e-mahmoudi.me>
2024-02-14 18:43:55 +0330

    mission on offers

--- src/models/offer/read.js
+++ src/models/offer/read.js
@@ -20,7 +20,8 @@ export const get = async (id) => {
       row_to_json(recipient.*) AS recipient,
       row_to_json(org.*) AS organization,
       row_to_json(a.*) AS applicant,
-      row_to_json(e.*) AS escrow
+      row_to_json(e.*) AS escrow,
+      row_to_json(m.*) AS mission
     FROM offers o
     JOIN projects p ON p.id=o.project_id
     JOIN identities offerer ON offerer.id=o.offerer_id
@@ -29,6 +30,7 @@ export const get = async (id) => {
     LEFT JOIN organizations org ON org.id=o.offerer_id
     LEFT JOIN applicants a ON a.id=o.applicant_id
     LEFT JOIN escrows e ON e.offer_id=o.id
+    LEFT JOIN missions m ON m.offer_id=o.id
     WHERE o.id=${id}
   `)
 }
@@ -53,6 +55,7 @@ export const getAll = async (identityId, { limit = 10, offset = 0, filter, sort 
     LEFT JOIN organizations org ON org.id=o.offerer_id
     LEFT JOIN applicants a ON a.id=o.applicant_id
     LEFT JOIN escrows e ON e.offer_id=o.id
+    LEFT JOIN missions m ON m.offer_id=o.id
     WHERE (o.recipient_id = ${identityId} OR o.offerer_id = ${identityId})
     ${filtering(filter, filterColumns, true, 'o')}
     ${sorting(sort, sortColumns)}

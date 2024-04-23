22b90ff3b7ccb40e30d858bec6aee8887754b054
jeyem <me@e-mahmoudi.me>
2024-04-23 18:06:59 +0330

    verification on identities and creating verification also

--- src/routes/credentials.js
+++ src/routes/credentials.js
@@ -22,6 +22,24 @@ import logger from '../utils/logging.js'
 export const router = new Router()
 
 router.post('/verifications', loginRequired, async (ctx) => {
+  const vc = await Credential.getRequestVerificationByIdentity(ctx.identity.id)
+  if (vc.present_id) {
+    const credential = await getPresentVerification(vc.present_id)
+    const rows = await Credential.searchSimilarVerification(credential)
+    if (rows.length < 1) {
+      ctx.body = await Credential.setVerificationApproved(vc.id, credential)
+      const referred = await Referring.get(ctx.identity.id)
+      if (referred) {
+        Event.push(Event.Types.NOTIFICATION, referred.referred_by_id, {
+          type: Notif.Types.REFERRAL_VERIFIED,
+          refId: ctx.identity.id,
+          parentId: ctx.identity.id,
+          identity: ctx.identity
+        })
+      }
+      return
+    }
+  }
   const connect = await createConnectURL(config.wallet.verification_callback)
   ctx.body = await Credential.requestVerification(ctx.identity.id, connect.id, connect.url)
 })
--- src/routes/identity.js
+++ src/routes/identity.js
@@ -1,11 +1,36 @@
 import Router from '@koa/router'
 import Identity from '../models/identity/index.js'
+import Notif from '../models/notification/index.js'
+import Referring from '../models/referring/index.js'
+import Event from '../services/events/index.js'
+import Credential from '../models/credentials/index.js'
+import { getPresentVerification } from '../services/wallet/index.js'
 import { loginRequired } from '../utils/middlewares/authorization.js'
 import { checkIdParams } from '../utils/middlewares/route.js'
 
 export const router = new Router()
 
 router.get('/', loginRequired, async (ctx) => {
+  
+  if (!ctx.user.identity_verified) {
+    const vc = await Credential.getRequestVerificationByIdentity(ctx.identity.id)
+    if (vc.present_id) {
+      const credential = await getPresentVerification(vc.present_id)
+      const rows = await Credential.searchSimilarVerification(credential)
+      if (rows.length < 1) {
+        await Credential.setVerificationApproved(vc.id, credential)
+        const referred = await Referring.get(ctx.identity.id)
+        if (referred) {
+          Event.push(Event.Types.NOTIFICATION, referred.referred_by_id, {
+            type: Notif.Types.REFERRAL_VERIFIED,
+            refId: ctx.identity.id,
+            parentId: ctx.identity.id,
+            identity: ctx.identity
+          })
+        }
+      }
+    }
+  }
   ctx.body = await Identity.getAll(ctx.user.id, ctx.identity.id)
 })
 

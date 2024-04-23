21d496828e5ff5cf28939e2029ad5a36e218dae1
jeyem <me@e-mahmoudi.me>
2024-04-23 14:12:29 +0330

    add mission on payments

--- src/services/payments/transaction.js
+++ src/services/payments/transaction.js
@@ -71,7 +71,8 @@ export const all = async (identityId, { limit = 10, offset = 0 }) => {
     SELECT 
       COUNT(*) OVER () as total_count, p.*, row_to_json(ref.*) as reference,
       row_to_json(i.*) as payer_identity,
-      row_to_json(i2.*) as receiver_identity
+      row_to_json(i2.*) as receiver_identity,
+      row_to_json(m.*) as mission
     FROM payments p
     LEFT JOIN payments ref ON ref.id=p.ref_trx
     JOIN escrows e ON e.payment_id=p.id OR e.payment_id=ref.id
@@ -79,6 +80,7 @@ export const all = async (identityId, { limit = 10, offset = 0 }) => {
     JOIN identities i ON i.id=pr.identity_id
     JOIN offers o ON o.id=e.offer_id
     JOIN identities i2 ON i2.id=o.recipient_id
+    LEFT JOIN missions m ON m.offer_id=o.id
     WHERE p.identity_id=${identityId} OR o.recipient_id=${identityId}
     ORDER BY p.created_at DESC
     LIMIT ${limit} OFFSET ${offset}
@@ -91,7 +93,8 @@ export const getOne = async (id, identityId) => {
     SELECT
       p.*, row_to_json(ref.*) as reference,
       row_to_json(i.*) as payer_identity,
-      row_to_json(i2.*) as receiver_identity
+      row_to_json(i2.*) as receiver_identity,
+      row_to_json(m.*) as mission
     FROM payments p
     LEFT JOIN payments ref ON ref.id=p.ref_trx
     JOIN escrows e ON e.payment_id=p.id OR e.payment_id=ref.id
@@ -99,6 +102,7 @@ export const getOne = async (id, identityId) => {
     JOIN identities i ON i.id=pr.identity_id
     JOIN offers o ON o.id=e.offer_id
     JOIN identities i2 ON i2.id=o.recipient_id
+    LEFT JOIN missions m ON m.offer_id=o.id
     WHERE p.id=${id} AND (p.identity_id=${identityId} OR o.recipient_id=${identityId})
   `)
   return rows

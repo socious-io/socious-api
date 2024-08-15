f72cf2d226934f7649b799f487c86511f022aa3f
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2024-08-16 00:32:46 +0330

    Fix: Apply Change Requests

--- src/routes/search.js
+++ src/routes/search.js
@@ -2,6 +2,7 @@ import Router from '@koa/router'
 import Search from '../services/search/index.js'
 import { loginOptional, loginRequired } from '../utils/middlewares/authorization.js'
 import { paginate } from '../utils/middlewares/requests.js'
+import { InternalServerError } from '../utils/errors.js'
 
 export const router = new Router()
 
@@ -10,10 +11,9 @@ router.post('/', loginOptional, paginate, async (ctx) => {
 })
 
 router.post('/v2', loginOptional, paginate, async (ctx) => {
-  if (ctx.searchClient) {
-    ctx.body = await ctx.searchService.search(ctx.request.body, ctx.paginate)
-    if (!ctx.guest) await Search.addHistory(ctx.request.body, ctx.identity.id)
-  }
+  if (!ctx.searchClient) throw new InternalServerError()
+  ctx.body = await ctx.searchService.search(ctx.request.body, ctx.paginate)
+  if (!ctx.guest) await Search.addHistory(ctx.request.body, ctx.identity.id)
 })
 
 router.get('/history', loginRequired, paginate, async (ctx) => {

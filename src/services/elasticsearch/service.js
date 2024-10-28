74e69b10d513b1eb3441acb870fe7ac47a343564
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2024-10-29 02:40:00 +0330

    feat: increasing number of insertion per query feat: add language filters feat: tune search results feat: add prioritization for the important fields fix: fixed some filters that was not working before

--- src/services/elasticsearch/service.js
+++ src/services/elasticsearch/service.js
@@ -8,35 +8,60 @@ export const search = async (body, pagination) => {
       projects: 'jobs',
       organizations: 'organizations'
     },
+    typeToFields = {
+      users: ['first_name^2', 'last_name^1'],
+      projects: ['title^2', 'description^1'],
+      organizations: [
+        'name^9',
+        'description^8',
+        'bio^7',
+        'email^6',
+        'shortname^5',
+        'mission^4',
+        'address^3',
+        'email^2',
+        'phone^1'
+      ]
+    },
     { q, type, filter } = body,
-    index = typeToIndex[type] + '_dev'
+    index = typeToIndex[type] + '_dev',
+    fields = typeToFields[type]
 
   if (!index) throw new BadRequestError(`type '${type}' is not valid`)
   await Data.SearchSchema.validateAsync(body)
 
   //make filters elastic-compliant
-  const filters = []
-  filter = filter??{}
+  let filters = []
+  filter = filter ?? {}
   for (const [key, value] of Object.entries(filter)) {
     const tempFilter = {}
 
     if (typeof value == 'string') {
-      tempFilter.term = {
-        [key]: value
-      }
+      filters.push({
+        match_phrase: {
+          [key]: value
+        }
+      })
     } else if (Array.isArray(value)) {
-      tempFilter.terms = {
-        [key]: value
-      }
+      const phrase_filters = []
+      value.forEach((val) =>
+        phrase_filters.push({
+          match_phrase: {
+            [key]: val
+          }
+        })
+      )
+      filters = [...filters, ...phrase_filters]
     } else if (typeof value == 'object') {
       const [op, opValue] = Object.entries(value)[0]
-      tempFilter.range = {
-        [key]: {
-          [op]: opValue
+      filters.push({
+        range: {
+          [key]: {
+            [op]: opValue
+          }
         }
-      }
+      })
     }
-    filters.push(tempFilter)
   }
 
   //setting filter parameters
@@ -49,7 +74,8 @@ export const search = async (body, pagination) => {
     queryDsl.bool.must = [
       {
         query_string: {
-          query: `*${q.trim()}*`
+          query: `*${q.trim()}*`,
+          fields
         }
       }
     ]

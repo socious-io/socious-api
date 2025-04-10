298ab8133a157e5f4ac3b1cf0383b83039ff79bf
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-04-10 23:37:16 +0330

    fix: fixing FCM

--- src/services/fcm/index.js
+++ src/services/fcm/index.js
@@ -1,37 +1,70 @@
 import axios from 'axios'
-import config from '../../config.js'
-import logger from '../../utils/logging.js'
-import Device from '../../models/device/index.js'
+import jwt from 'jsonwebtoken'
+import config from './config'
+import logger from './logger'
 
-const URL = 'https://fcm.googleapis.com/fcm/send'
+// Get OAuth 2.0 access token manually using JWT
+const getAccessToken = async () => {
+  const { client_email, private_key, project_id } = config.fcm.serviceAccount
 
-const HEADERS = {
-  Authorization: `key=${config.fcm.key}`
+  const now = Math.floor(Date.now() / 1000)
+  const payload = {
+    iss: client_email,
+    scope: 'https://www.googleapis.com/auth/firebase.messaging',
+    aud: 'https://oauth2.googleapis.com/token',
+    iat: now,
+    exp: now + 3600
+  }
+
+  const jwtToken = jwt.sign(payload, private_key, { algorithm: 'RS256' })
+
+  const { data } = await axios.post(
+    'https://oauth2.googleapis.com/token',
+    new URLSearchParams({
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: jwtToken
+    }),
+    { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
+  )
+
+  return data.access_token
 }
 
 export const simplePush = async ({ tokens, notification, data = {}, options = {} }) => {
-  if (tokens.length < 1) return
+  if (!tokens.length) return
 
-  const body = {
-    ...options,
-    registration_ids: tokens,
-    notification,
-    data
+  const accessToken = await getAccessToken()
+  const { project_id } = config.fcm.serviceAccount
+  const HEADERS = {
+    Authorization: `Bearer ${accessToken}`
   }
 
-  try {
-    const response = await axios.post(URL, body, { headers: HEADERS })
-    logger.info(JSON.stringify(body))
-    logger.info(response.data)
+  const failedTokens = []
 
-    const failedTokens = []
-
-    for (const i in response.data.results) {
-      if (response.data.results[i].error) failedTokens.push(tokens[i])
+  for (const token of tokens) {
+    const body = {
+      message: {
+        token,
+        notification,
+        data,
+        android: options.android,
+        apns: options.apns,
+        webpush: options.webpush
+      }
     }
 
-    await Device.cleanup(failedTokens)
-  } catch (err) {
-    logger.error(err)
+    try {
+      const response = await axios.post(
+        `https://fcm.googleapis.com/v1/projects/${project_id}/messages:send`,
+        body,
+        { headers: HEADERS }
+      )
+      logger.info(`Sent to ${token}: ${JSON.stringify(response.data)}`)
+    } catch (err) {
+      logger.error(`Error sending to ${token}:`, err.response?.data || err.message)
+      failedTokens.push(token)
+    }
   }
+
+  await Device.cleanup(failedTokens)
 }
